0a1d1aeb6e3b4c15247b87b77f4e702d83cf306b
leothebeagle <a.kosber@gmail.com>
2020-10-15 20:40:37 -0400

    Associate cart and user models via user_cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,2 +1,4 @@
 class Cart < ApplicationRecord
+    has_one :user_cart 
+    has_one :user, through: :user_cart
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    has_one :user_cart 
+    has_one :cart, through: :user_cart
 end
