0f1485e0841d369d2b700b4f7cc9ed4cc8ccbbea
leothebeagle <a.kosber@gmail.com>
2020-10-15 21:14:59 -0400

    Add password digest column to sellers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_000825) do
+ActiveRecord::Schema.define(version: 2020_10_16_011354) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "cart_id"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_000825) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "user_carts", force: :cascade do |t|
