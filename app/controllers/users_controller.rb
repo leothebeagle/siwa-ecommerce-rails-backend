408205dbf103562a0b408df023da31b594f4f172
leothebeagle <a.kosber@gmail.com>
2020-10-20 02:26:03 -0400

    Implement strong_params with correct naming

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
     def create 
+        user = User.create(user_params)
         binding.pry
+    end
+
+
+    private
+ 
+    def user_params
+      params.require(:user).permit(:first_name, :last_Name, :email, :password, :password_confirmation)
     end
 
 end
