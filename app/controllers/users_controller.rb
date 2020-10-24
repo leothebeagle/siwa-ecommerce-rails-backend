c3a10dd9328114848cb6493bf0f29e7021041888
leothebeagle <a.kosber@gmail.com>
2020-10-23 22:49:51 -0400

    Correct typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     private
  
     def user_params
-      params.require(:user).permit(:first_name, :last_Name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
