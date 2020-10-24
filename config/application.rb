a0348dab572b8219d0942ec3481b1ddce1ae1ea6
leothebeagle <a.kosber@gmail.com>
2020-10-23 20:03:30 -0400

    Include the middleware needed to use cookies

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,9 @@ module SiwaRailsBackend
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -42,8 +44,5 @@ module SiwaRailsBackend
           :credentials => true
       end
     end
-
-    ActionDispatch::Session::CookieStore
-
   end
 end
