34a7aefb8df5819ffa837f42bfc43b2584071223
leothebeagle <a.kosber@gmail.com>
2020-10-29 13:35:16 -0400

    Include ActionController::Cookies

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module SiwaRailsBackend
     config.load_defaults 5.2
     
     config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use ActionDispatch::Session::CookieStore  
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
