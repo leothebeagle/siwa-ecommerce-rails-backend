758dade030b838807da9868a7876ef1ca5ca1ad4
leothebeagle <a.kosber@gmail.com>
2020-10-29 18:23:21 -0400

    Build create action in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
 
+    def create 
+        user = User.find_by(email: params[:user][:email])
+        binding.pry
+        if user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            render json: {
+                logged_in: true,
+                user: user
+              }
+        end
+    end
+
     def logout
         reset_session
         render json: {
