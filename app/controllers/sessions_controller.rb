c098b2074b42aa9d51d997ca9e8471602ad90644
leothebeagle <a.kosber@gmail.com>
2020-10-29 18:26:09 -0400

    Remove binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
     def create 
         user = User.find_by(email: params[:user][:email])
-        binding.pry
+     
         if user.authenticate(params[:user][:password])
             session[:user_id] = user.id
             render json: {
