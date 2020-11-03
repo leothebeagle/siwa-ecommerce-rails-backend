c443cd5acf0141d160c70f07b0c98c5336ed9b0a
leothebeagle <a.kosber@gmail.com>
2020-11-03 18:10:57 -0500

    Serve items in index action of ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,9 @@
 class ItemsController < ApplicationController
     def index 
         binding.pry
+        items = Item.all
+        render json: {
+            items: items
+        }
     end
 end
