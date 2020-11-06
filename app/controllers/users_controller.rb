490c076176ddf6a3d831edf938f6f51526077d17
leothebeagle <a.kosber@gmail.com>
2020-11-05 19:15:06 -0500

    Give controllers access to incoming cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  include ActionController::Cookies
-
+  
     def create 
         user = User.create(user_params)
         session[:user_id] = user.id
