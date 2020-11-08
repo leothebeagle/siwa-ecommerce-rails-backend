6b88d712787eeded0c58efe1d1a3e09fc2966e60
leothebeagle <a.kosber@gmail.com>
2020-11-08 18:44:11 -0500

    Remove binding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
      
         if user.authenticate(params[:user][:password])
             session[:user_id] = user.id
-            binding.pry
             render json: {
                 logged_in: true,
                 user: user,
@@ -13,16 +12,14 @@ class SessionsController < ApplicationController
                     id: current_cart.id,
                     items: current_cart.items
                 }
-              }
+            }
         end
     end
 
     def logout
         reset_session
-        binding.pry
         render json: {
             logged_out: true
         }
     end
-
 end
