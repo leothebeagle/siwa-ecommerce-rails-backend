98b6cee08d84dc7c562d7b965fb8711a5a500709
leothebeagle <a.kosber@gmail.com>
2020-11-07 20:18:43 -0500

    Write a function to find the current cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,8 @@ class ApplicationController < ActionController::API
         @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end 
 
+    def current_cart
+        @current_cart ||= current_user.cart
+    end
+
 end
