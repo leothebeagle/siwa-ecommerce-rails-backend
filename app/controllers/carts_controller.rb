e320a3c963bf66ce11af31fc68876fcaf87dbd28
leothebeagle <a.kosber@gmail.com>
2020-11-08 18:44:36 -0500

    Add Item to cart for guest user

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,24 @@
 class CartsController < ApplicationController
     def add_item_to_cart
+        binding.pry
+        cart = Cart.find(params["cart_id"]) if params["cart_id"]
+        item = Item.find(params["item"]["id"]) if params["item"]["id"]
+
+        if cart 
+            cart.items << item
+            cart.save
+        else 
+            cart = Cart.create 
+            cart.items << item 
+            cart.save 
+        end
+        binding.pry
         render json: {
-            cart_id: "test",
-            cart_items: "test"
+            cart: {
+                id: cart.id,
+                items: cart.items
+            }
         }
+        
     end
 end
