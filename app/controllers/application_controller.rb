b323cfd3ba31fc00c36e0a297f091e0017059939
leothebeagle <a.kosber@gmail.com>
2020-11-11 16:16:23 -0500

    Write methods to check cart and session cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,16 @@ class ApplicationController < ActionController::API
         current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
     end
 
+    def check_session_cookie
+        cookie_session_id = cookies.encrypted[:_session_id]["session_id"]
+        cookie_user_id = cookies.encrypted[:_session_id]["user_id"]
+
+        true if cookie_session_id == session.id && cookie_user_id == current_user.id   
+    end
+
+    def check_cart_cookie
+        cookie_cart_id = cookies["cart_id"]
+        true if cookie_cart_id == session[:cart_id]
+    end
+
 end
