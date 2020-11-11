129182ce7f233b01167d0c31d6ec709467c9f26d
leothebeagle <a.kosber@gmail.com>
2020-11-11 16:04:34 -0500

    Find current card based on cart id in the session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
     
+    @test_variable = "I am an instance variable. can you access me?"
+
     def current_user 
-        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        current_user ||= User.find(session[:user_id]) if session[:user_id]
     end 
 
     def current_cart
-        @current_cart ||= current_user.cart if current_user
+        current_cart ||= Cart.find(session[:cart_id]) if session[:cart_id]
     end
 
 end
