a1fa7ca8b1e63184b4e18200fae24af2477f231e
leothebeagle <a.kosber@gmail.com>
2020-11-12 14:10:33 -0500

    Complete create action in OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,23 @@
 class OrdersController < ApplicationController
-    def create 
+    def create
+        binding.pry 
+        if check_cart_cookie
+            order = Order.create
+            order.cart = current_cart
+            new_cart = Cart.create 
+            session[:cart_id] = new_cart.id
+            cookies[:cart_id] = new_cart.id
+        end
+        
         binding.pry
+
+
         render json: {
-            test: "this is a test message from orders#create"
+            cart: {
+                id: current_cart.id,
+                items: current_cart.items
+            }
         }
+        
     end
 end
