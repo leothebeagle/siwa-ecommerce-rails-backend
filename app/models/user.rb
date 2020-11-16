69f183921e6f7a98538f13c01903efdfe1150d7e
leothebeagle <a.kosber@gmail.com>
2020-11-15 23:41:46 -0500

    Change association between user and carts.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_one :user_cart 
-    has_one :cart, through: :user_cart
+    has_many :user_carts 
+    has_one :carts, through: :user_carts
 end
