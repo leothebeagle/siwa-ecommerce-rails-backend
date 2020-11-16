ac095af2ba43a673030fed6ae0616bad3caf08d9
leothebeagle <a.kosber@gmail.com>
2020-11-15 23:42:24 -0500

    Setup conditionals to create cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,20 +1,27 @@
 class OrdersController < ApplicationController
+    def setup_cart_for_user
+        if current_user
+            new_cart = Cart.create 
+            new_cart.user = current_user if current_user
+            session[:cart_id] = new_cart.id
+            cookies[:cart_id] = new_cart.id 
+        end  
+    end
+    
     def create
         
         if check_cart_cookie
             order = Order.new
             order.cart = current_cart
             order.save
-            new_cart = Cart.create 
-            session[:cart_id] = new_cart.id
-            cookies[:cart_id] = new_cart.id
+            setup_cart_for_user
         end
         
         render json: {
             cart: {
-                id: current_cart.id,
-                items: current_cart.items,
-                total: current_cart_total
+                id: current_user ? current_cart.id : "",
+                items: current_user ? current_cart.items : [],
+                total: current_user ?  current_cart_total : 0
             }
         }
         
