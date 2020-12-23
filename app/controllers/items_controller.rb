3d0d9aefec573896139286cd032193b15efa8a20
leothebeagle <a.kosber@gmail.com>
2020-12-23 14:26:37 -0500

    Write conditional to filter items in index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,13 @@
 class ItemsController < ApplicationController
-    def index 
-        items = Item.all
+    def index
+        if params["q"] == "greater"
+            items = Item.where(["price > ?", "50"])
+        elsif params["q"] == "less"
+            items = Item.where(["price < ?", "50"])
+        else 
+            items = Item.all
+        end
+
         render json: {
             items: items
         }
